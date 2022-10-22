45ef35319f8402d9b175c729d52eb97875b64fc7
Mikael Eriksson <m.eriksson@gmx.com>
2022-10-22 19:44:47 +0200

    Uppdaterade Tobias branch med Maven, så JavaFx ska funka med SDK 17.

---
+++ SankaSkepp/src/main/java/com/grupp2/sankaskepp/TheCell.java
@@ -0,0 +1,19 @@
+package com.grupp2.sankaskepp;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class TheCell extends Rectangle {
+
+    public int x, y;
+    // public boolean wasShot = false;
+    private GameBoard board;
+
+    public TheCell(int x, int y, GameBoard board) {
+        super(30, 30);
+        this.x = x;
+        this.y = y;
+        this.board = board;
+        setFill(Color.AQUA);
+        setStroke(Color.BLACK);
+    }
+}
