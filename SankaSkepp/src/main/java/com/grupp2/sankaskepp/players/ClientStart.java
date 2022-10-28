4cc3fe4eb99cc9ca4395dc96c37eef3a528bee65
Wei <wei.li@edu.edugrade.se>
2022-10-28 22:32:14 +0200

    Added client and server classes under players package

---
+++ SankaSkepp/src/main/java/com/grupp2/sankaskepp/players/ClientStart.java
@@ -0,0 +1,9 @@
+package com.grupp2.sankaskepp.players;
+
+import java.io.IOException;
+
+public class ClientStart {public static void main(String[] args) throws IOException {
+    Client client = new Client();
+    client.start();
+}
+}
--- SankaSkepp/src/main/java/module-info.java
+++ SankaSkepp/src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.grupp2.sankaskepp {
 
     opens com.grupp2.sankaskepp to javafx.fxml;
     exports com.grupp2.sankaskepp;
+    exports com.grupp2.sankaskepp.players;
+    opens com.grupp2.sankaskepp.players to javafx.fxml;
 }
