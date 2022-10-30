8fc44761e6c2150ba9d3e3ef39b1841b7e1117a8
ToJohansson <115701119+ToJohansson@users.noreply.github.com>
2022-10-30 16:43:50 +0100

    har fört samman våra koder nu, behövde banta ner min rejält :P finns lite saker att fixa innan det är perfekt. båtar placeras ibland för nära varandra och det blir också oändliga loopar.

--- SankaSkepp/src/main/java/com/grupp2/sankaskepp/HelloApplication.java
+++ SankaSkepp/src/main/java/com/grupp2/sankaskepp/HelloApplication.java
@@ -1,64 +1,64 @@
 package com.grupp2.sankaskepp;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.util.Random;
-import java.io.IOException;
-
-/**
- * För att köra , höger-klicka på pom.xml filen och välja "add as Maven project"  så ska allt rött försvinna.
- */
 
 public class HelloApplication extends Application {
-
     private boolean gameIsOn = false;
     private GameBoard attackerBoard, defenderBoard;
 
-    private boolean attackerTurn = false;
-    private boolean defenderTurn = false;
     private Random random = new Random();
 
+
+    public static void main(String[] args) {
+
+        // startar fx
+        launch(args);
+    }
+
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    public void start(Stage primaryStage) throws Exception {
         Scene scene = new Scene(createContent());
-        primaryStage.setTitle("Amazing battleship game");
+        primaryStage.setTitle("Battleship");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
-
     private Parent createContent() {
         BorderPane root = new BorderPane();
         root.setPrefSize(600, 700);
-        // KONTROLLPANEL?
-        root.setRight(new TextArea("Kanske en kontrollpanel här som håller koll vilka kordinater " +
-                "                   som lades och om träff eller miss"));
-        attackerBoard = new GameBoard(true, false);
-        defenderBoard = new GameBoard(false, true);
 
-        VBox vBox = new VBox(50, attackerBoard, defenderBoard);
-        vBox.setAlignment(Pos.TOP_CENTER);
+
+        Boat boat = new Boat();
+        PlaceBoats placeBoats = new PlaceBoats();
+        boat.createBoats();
+        placeBoats.initializeGridArray();
+        placeBoats.newTest(boat.getBoats());
+
+        attackerBoard = new GameBoard(boat);
+
+        // ***************************************
+
+        Boat boat2 = new Boat();
+        PlaceBoats placeBoats2 = new PlaceBoats();
+        boat2.createBoats();
+        placeBoats2.initializeGridArray();
+        placeBoats2.newTest(boat2.getBoats());
+
+        defenderBoard = new GameBoard(boat2);
+
+        VBox vBox = new VBox(10, attackerBoard, defenderBoard);
+        vBox.setAlignment(Pos.TOP_LEFT);
 
         root.setCenter(vBox);
 
         return root;
-        // hej
     }
 }
