5dff07c4752468698f62b9e77a06c43eb2321d63
karin <karin.lamell@edu.edugrade.se>
2022-11-10 08:45:45 +0100

    Ändrat if-satserna till switch för att få renare kod. Båtlistan inlagd. Mycket mer logik återstår!

--- SankaSkepp/src/main/java/com/grupp2/sankaskepp/Remaining/MainRemain.java
+++ SankaSkepp/src/main/java/com/grupp2/sankaskepp/Remaining/MainRemain.java
@@ -43,43 +43,42 @@ public class MainRemain {
 
         Boat EnemyBoats = new Boat();
 
+
+
         //while EnemyBoats[] == !0; - formulera logiken mer korrekt
-
-        if (Character.toString(this.enemyShot.charAt(0)).equals("i")) {
-
-            Collections.shuffle(remainingXYspots);  // return Random xyValue from remainingXYspots
-            enemyGameBoard.hitList.add(enemyGameBoard.remainingXYspots.get(0));  // add new XyValue to hitList
-            enemyGameBoard.remainingXYspots.remove(0);  // remove xyValue from remainingXYspots
+        switch (Character.toString(this.enemyShot.charAt(0))) {
+        case "i":   Collections.shuffle(remainingXYspots);  // return Random xyValue from remainingXYspots
+                    enemyGameBoard.hitList.add(enemyGameBoard.remainingXYspots.get(0));  // add new XyValue to hitList
+                    enemyGameBoard.remainingXYspots.remove(0);  // remove xyValue from remainingXYspots
 
 
-        } else if (Character.toString(this.enemyShot.charAt(0)).equals("m")) {
-
-            if (enemyGameBoard.hitList.size() < 2) {
-                enemyGameBoard.hitList.clear();   // clear hitList
-                Collections.shuffle(remainingXYspots);  // return Random xyValue from remainingXYspots
-                enemyGameBoard.hitList.add(enemyGameBoard.remainingXYspots.get(0));  // add new XyValue to hitList
-                enemyGameBoard.remainingXYspots.remove(0);  // remove xyValue from remainingXYspots
-            } else if (enemyGameBoard.hitList.size() > 1) { //använd logik för nästa skott,
-                enemyGameBoard.hitList.remove(enemyGameBoard.hitList.size() - 1); //ta bort värdet för sista index
+            case "m":
+                if (enemyGameBoard.hitList.size() < 2) {
+                    enemyGameBoard.hitList.clear();   // clear hitList
+                    Collections.shuffle(remainingXYspots);  // return Random xyValue from remainingXYspots
+                    enemyGameBoard.hitList.add(enemyGameBoard.remainingXYspots.get(0));  // add new XyValue to hitList
+                    enemyGameBoard.remainingXYspots.remove(0);  // remove xyValue from remainingXYspots
+            }
+                else if (enemyGameBoard.hitList.size() > 1) { //använd logik för nästa skott,
+                    enemyGameBoard.hitList.remove(enemyGameBoard.hitList.size() - 1); //ta bort värdet för sista index
             }
 
 
-        } else if (Character.toString(this.enemyShot.charAt(0)).equals("h")) {
+            case "h":
+                    // kolla om hitList>1 isåfall ta nästa möjliga koordinat i linje med två tidigare
+                    // find position next to XyValue in hitList, will be  next shot
+                     enemyGameBoard.remainingXYspots.remove(0);  // remove xyValue from remainingXYspots
 
-            // kolla om hitList>1 isåfall ta nästa möjliga koordinat i linje med två tidigare
-            // find position next to XyValue in hitList, will be  next shot
-            enemyGameBoard.remainingXYspots.remove(0);  // remove xyValue from remainingXYspots
+            case "s":
+                    /*   EnemyBoats.length tells which boat was sunk
+                         logic to clear safe points around boat - add positions to guardedSpots[]
+                         remove boat from ListEnemyBoats[]*/
 
-        } else if (Character.toString(enemyShot.charAt(0)).equals("s")) {
-
-            // EnemyBoats.length tells which boat was sunk
-            // logic to clear safe points around boat - add positions to guardedSpots[]
-            // remove boat from ListEnemyBoats[]
-            // remove guardedSpots from remainingXYspots
-            enemyGameBoard.hitList.clear();   // clear hitList
-            Collections.shuffle(remainingXYspots);  // return Random xyValue from remainingXYspots
-            enemyGameBoard.hitList.add(enemyGameBoard.remainingXYspots.get(0));  // add new XyValue to hitList
-            enemyGameBoard.remainingXYspots.remove(0);  // remove xyValue from remainingXYspots
+                    // remove guardedSpots from remainingXYspots
+                    enemyGameBoard.hitList.clear();   // clear hitList
+                    Collections.shuffle(remainingXYspots);  // return Random xyValue from remainingXYspots
+                    enemyGameBoard.hitList.add(enemyGameBoard.remainingXYspots.get(0));  // add new XyValue to hitList
+                    enemyGameBoard.remainingXYspots.remove(0);  // remove xyValue from remainingXYspots
 
         }
     }
