aea61a84abf16f06c2afa7985c894a0365248f37
Mikael Eriksson <m.eriksson@gmx.com>
2022-11-14 11:49:27 +0100

    Ändrade i module-info

--- SankaSkepp/src/main/java/module-info.java
+++ SankaSkepp/src/main/java/module-info.java
@@ -3,14 +3,14 @@ module com.grupp2.sankaskepp {
     requires javafx.fxml;
 
     //opens com.grupp2.sankaskepp to javafx.fxml;
-    exports com.grupp2.sankaskepp.players;
-    opens com.grupp2.sankaskepp.players to javafx.fxml;
-    exports com.grupp2.sankaskepp.Bastian_Tobias_Anna;
     opens com.grupp2.sankaskepp.Bastian_Tobias_Anna to javafx.fxml;
+    exports com.grupp2.sankaskepp.players_Wei_Mikael;
+    exports com.grupp2.sankaskepp.Bastian_Tobias_Anna;
+    //opens com.grupp2.sankaskepp.players_Wei_Mikael to javafx.fxml;
 
-    opens com.grupp2.sankaskepp to javafx.fxml;
+    //opens com.grupp2.sankaskepp to javafx.fxml;
     //exports com.grupp2.sankaskepp.AnnaGUI;
-    opens com.grupp2.sankaskepp.AnnaGUI to javafx.fxml;
-    exports com.grupp2.sankaskepp.CreateAndSetBoats;
-    opens com.grupp2.sankaskepp.CreateAndSetBoats to javafx.fxml;
+    //opens com.grupp2.sankaskepp.AnnaGUI to javafx.fxml;
+    //exports com.grupp2.sankaskepp.CreateAndSetBoats;
+    //opens com.grupp2.sankaskepp.CreateAndSetBoats to javafx.fxml;
 }
