d1d5adaa0875cfb3df410ca57b59288670c80650
ASahrlingHamm <anna.sahrling-hamm@edu.edugrade.se>
2022-11-25 12:04:14 +0100

    Knapp

--- SankaSkepp/src/main/java/com/grupp2/sankaskepp/Bastian_Tobias_Anna/BattleshipGame.java
+++ SankaSkepp/src/main/java/com/grupp2/sankaskepp/Bastian_Tobias_Anna/BattleshipGame.java
@@ -15,8 +15,12 @@ import java.io.IOException;
 public class BattleshipGame extends Application {
 
     private GameBoard youBoard, enemyBoard;
-    private HelloController helloController;
+
+    Text historyText = new Text("History");
+    private HelloController helloController = new HelloController(historyText);
     private DropShadow dropShadow = new DropShadow();
+
+    private Button startButton = new Button("Start");
 
     @Override
     public void start(Stage primaryStage) throws IOException, InterruptedException {
@@ -38,104 +42,22 @@ public class BattleshipGame extends Application {
         VBox root = new VBox();
         root.setPrefSize(1200, 760);
 
-        HBox history = new HBox();
-        Text historyText = new Text("History");
-        historyText.setFill(Color.web("37a8b7"));
-        historyText.setEffect(dropShadow);
-        helloController = new HelloController(historyText);
-        history.getChildren().add(historyText);
-        history.getStyleClass().add("history");
-
-        HBox submarine = new HBox();
-        Text submarineLabel = new Text("4 submarines");
-        submarineLabel.setFill(Color.web("37a8b7"));
-        submarineLabel.setEffect(dropShadow);
-        submarine.setAlignment(Pos.TOP_RIGHT);
-        submarine.getChildren().add(submarineLabel);
-
-        HBox cruiser = new HBox();
-        Text cruiserLabel = new Text("3 cruisers");
-        cruiserLabel.setFill(Color.web("37a8b7"));
-        cruiserLabel.setEffect(dropShadow);
-        cruiser.setAlignment(Pos.TOP_RIGHT);
-        cruiser.getChildren().add(cruiserLabel);
-
-        HBox battleship = new HBox();
-        Text battleshipLabel = new Text("2 battleships");
-        battleshipLabel.setFill(Color.web("37a8b7"));
-        battleshipLabel.setEffect(dropShadow);
-        battleship.setAlignment(Pos.TOP_RIGHT);
-        battleship.getChildren().add(battleshipLabel);
-
-        HBox carrier = new HBox();
-        Text carrierLabel = new Text("1 carrier");
-        carrierLabel.setFill(Color.web("37a8b7"));
-        carrierLabel.setEffect(dropShadow);
-        carrier.setAlignment(Pos.BOTTOM_RIGHT);
-        carrier.getChildren().add(carrierLabel);
-
-        VBox boatBox = new VBox();
-        submarine.setPadding(new Insets(70, 180, 0, 0));
-        cruiser.setPadding(new Insets(78, 80, 0, 0));
-        battleship.setPadding(new Insets(405, 80, 0, 0));
-        carrier.setPadding(new Insets(90, 120, 0, 0));
-        boatBox.getChildren().addAll(submarine, cruiser, battleship, carrier);
-
-
-
-        Region filler = new Region();
-        filler.setPrefHeight(50);
-        HBox.setHgrow(filler, Priority.ALWAYS);
-
-        VBox you = new VBox();
-        you.getStyleClass().add("enemyAndYou");
-        you.setAlignment(Pos.BOTTOM_LEFT);
-
-        VBox enemy = new VBox();
-        enemy.getStyleClass().add("enemyAndYou");
-        enemy.setAlignment(Pos.BOTTOM_RIGHT);
-
-        Text youLabel = new Text("You");
-        youLabel.setFill(Color.web("#b938e2"));
-        youLabel.setEffect(dropShadow);
-        Text enemyLabel = new Text("Enemy");
-        enemyLabel.setFill(Color.web("#b938e2"));
-        enemyLabel.setEffect(dropShadow);
 
         // Tobias { ***********
         this.enemyBoard = helloController.enemyBoard;
         this.youBoard = helloController.youBoard;
         // ********  } Tobias
 
-        you.getChildren().addAll(youLabel, youBoard);
-        enemy.getChildren().addAll(enemyLabel, enemyBoard);
-
 
         Button startButton = new Button("Start");
         startButton.setEffect(dropShadow);
-        Button stopButton = new Button("Stop");
-        stopButton.setEffect(dropShadow);
-
         startButton.setOnAction(helloController.startButtonHandler());
-
-
-        VBox startButtonBox = new VBox(startButton);
-        startButtonBox.getStyleClass().add("startButtonBox");
-        VBox stopButtonBox = new VBox(stopButton);
-        stopButtonBox.getStyleClass().add("stopButtonBox");
-
         startButton.setAlignment(Pos.CENTER);
 
 
-        HBox bottomBox = new HBox(40, startButtonBox);
 
-        HBox boards = new HBox(50, you, enemy);
-        boards.setAlignment(Pos.CENTER);
-        root.getChildren().addAll(createTitle(), history, filler, boards, bottomBox);
-        bottomBox.setAlignment(Pos.BOTTOM_CENTER);
-        history.setAlignment(Pos.BASELINE_CENTER);
-        boatBox.setAlignment(Pos.BASELINE_RIGHT);
-        StackPane stack = new StackPane(boatBox, root);
+        root.getChildren().addAll(createTitle(), createHistory(), createFiller(), createBoards(),createBottomBox());
+        StackPane stack = new StackPane(createBoatBox(), root);
 
         return stack;
     }
@@ -151,4 +73,140 @@ public class BattleshipGame extends Application {
         title.getChildren().add(titleLabel);
         return title;
     }
+
+    private HBox createHistory() {
+        HBox history = new HBox();
+        historyText.setFill(Color.web("37a8b7"));
+        historyText.setEffect(dropShadow);
+        helloController = new HelloController(historyText);
+        history.getChildren().add(historyText);
+        history.getStyleClass().add("history");
+        history.setAlignment(Pos.BASELINE_CENTER);
+        return history;
+    }
+
+    private HBox createSubmarine() {
+        HBox submarine = new HBox();
+        Text submarineLabel = new Text("4 submarines");
+        submarineLabel.setFill(Color.web("37a8b7"));
+        submarineLabel.setEffect(dropShadow);
+        submarine.setAlignment(Pos.TOP_RIGHT);
+        submarine.getChildren().add(submarineLabel);
+        submarine.setPadding(new Insets(70, 180, 0, 0));
+        return submarine;
+    }
+        private HBox createCruiser() {
+        HBox cruiser = new HBox();
+        Text cruiserLabel = new Text("3 cruisers");
+        cruiserLabel.setFill(Color.web("37a8b7"));
+        cruiserLabel.setEffect(dropShadow);
+        cruiser.setAlignment(Pos.TOP_RIGHT);
+        cruiser.getChildren().add(cruiserLabel);
+        cruiser.setPadding(new Insets(78, 80, 0, 0));
+        return cruiser;
+        }
+
+        private HBox createBattleship() {
+        HBox battleship = new HBox();
+        Text battleshipLabel = new Text("2 battleships");
+        battleshipLabel.setFill(Color.web("37a8b7"));
+        battleshipLabel.setEffect(dropShadow);
+        battleship.setAlignment(Pos.TOP_RIGHT);
+        battleship.getChildren().add(battleshipLabel);
+        battleship.setPadding(new Insets(405, 80, 0, 0));
+        return battleship;
+        }
+
+        private HBox createCarrier() {
+        HBox carrier = new HBox();
+        Text carrierLabel = new Text("1 carrier");
+        carrierLabel.setFill(Color.web("37a8b7"));
+        carrierLabel.setEffect(dropShadow);
+        carrier.setAlignment(Pos.BOTTOM_RIGHT);
+        carrier.getChildren().add(carrierLabel);
+        carrier.setPadding(new Insets(90, 120, 0, 0));
+        return carrier;
+        }
+
+        private VBox createBoatBox() {
+        VBox boatBox = new VBox();
+        boatBox.getChildren().addAll(createSubmarine(), createCruiser(), createBattleship(), createCarrier());
+        boatBox.setAlignment(Pos.BASELINE_RIGHT);
+        return boatBox;
+        }
+
+        private Region createFiller() {
+        Region filler = new Region();
+        filler.setPrefHeight(50);
+        HBox.setHgrow(filler, Priority.ALWAYS);
+        return filler;
+        }
+
+        private VBox createYou() {
+        VBox you = new VBox();
+        Text youLabel = new Text("You");
+        youLabel.setFill(Color.web("#b938e2"));
+        youLabel.setEffect(dropShadow);
+        you.getStyleClass().add("enemyAndYou");
+        you.setAlignment(Pos.BOTTOM_LEFT);
+        you.getChildren().addAll(youLabel, youBoard);
+        return you;
+        }
+
+        private VBox createEnemy() {
+        VBox enemy = new VBox();
+        Text enemyLabel = new Text("Enemy");
+        enemyLabel.setFill(Color.web("#b938e2"));
+        enemyLabel.setEffect(dropShadow);
+        enemy.getStyleClass().add("enemyAndYou");
+        enemy.setAlignment(Pos.BOTTOM_RIGHT);
+        enemy.getChildren().addAll(enemyLabel, enemyBoard);
+        return enemy;
+        }
+
+        private VBox createStartButtonBox() {
+        startButton.setEffect(dropShadow);
+        startButton.setOnAction(helloController.startButtonHandler());
+        startButton.setAlignment(Pos.CENTER);
+        VBox startButtonBox = new VBox(startButton);
+        startButtonBox.getStyleClass().add("startButtonBox");
+        return startButtonBox;
+        }
+
+        private HBox createBottomBox() {
+        HBox bottomBox = new HBox(40, createStartButtonBox());
+        bottomBox.setAlignment(Pos.BOTTOM_CENTER);
+        return bottomBox;
+        }
+
+        private HBox createBoards() {
+        HBox boards = new HBox(50, createYou(), createEnemy());
+        boards.setAlignment(Pos.CENTER);
+        return boards;
+        }
+
+        private Button createStartButton() {
+        Button startButton = new Button("Start");
+        startButton.setEffect(dropShadow);
+        startButton.setOnAction(helloController.startButtonHandler());
+        startButton.setAlignment(Pos.CENTER);
+        return startButton;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
